e6afd8856594f8770beab3a99892a354f8973869
Nilin Abrahamsen <nilin@berkeley.edu>
2023-12-08 14:47:43 -0800

    print total time every pause

--- stopwatch.py
+++ stopwatch.py
@@ -5,13 +5,18 @@ import threading
 import sys
 class Timer:
 
+    def __init__(self):
+        self.start_time = 0
+        self.intervals=[]
+        self.running=False
+
     def start(self):
         self.start_time = time.time()
         self.running=True
 
     def stop(self):
         self.running=False
-        self.dt=time.time() - self.start_time
+        self.intervals.append(time.time() - self.start_time)
 
     def print_time(self):
         while self.running:
@@ -23,22 +28,18 @@ class Timer:
     def get_time_till_Enter(self):
         return self.stop()
 
-    @staticmethod
-    def save_time(intervals,fn):
+    def save_time(self,fn):
         name=os.path.join('stopwatchtimes',fn+'.txt')
         os.makedirs(os.path.dirname(name), exist_ok=True)
-        for dt in intervals:
+        for dt in self.intervals:
             with open(name, 'a') as f:
                 f.write(str(dt)+'\n')
         print(f'Saved splits to \n{name}')
         return name
 
-    @staticmethod
-    def print_total_time(name):
-        with open(name, 'r') as f:
-            intervals=[float(line.strip()) for line in f]
-        T=sum(intervals)
-        print(f'Total time\n'+str(datetime.timedelta(seconds=T)))
+    def print_total_time(self):
+        T=sum(self.intervals)
+        print(f'\nTotal time\n'+str(datetime.timedelta(seconds=T)))
 
 if __name__ == '__main__':
 
@@ -46,6 +47,7 @@ if __name__ == '__main__':
     intervals=[]
 
     while True:
+        timer.print_total_time()
         fn=input("\nPress Enter to start,\n"+\
                  "q+Enter to quit,\n"+\
                  "or enter [name] to save splits.\n")
@@ -60,5 +62,4 @@ if __name__ == '__main__':
         threading.Thread(target=timer.print_time).start()
         input('Press Enter to pause.\n')
         timer.stop()
-        intervals.append(timer.dt)
 
